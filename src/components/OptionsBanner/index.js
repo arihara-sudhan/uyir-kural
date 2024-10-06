9f2edd30ae418280dfc56cc81f99734d36817c2e
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-10-06 12:09:16 +0530

    Kural Changes

---
+++ src/components/OptionsBanner/index.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Meta from "./meta.json";
+import style from "./OptionsBanner.module.css";
+
+export default function MetaSelector(props) {
+    const [paal, setPaal] = useState("அறம்");
+    const [iyal, setIyal] = useState("பாயிரவியல்");
+
+    const paalOptions = Object.keys(Meta);
+    const iyalOptions = paal in Meta ? Object.keys(Meta[paal]) : [];
+    const itemOptions = paal in Meta && iyal in Meta[paal] ? Meta[paal][iyal] : [];
+
+    return (
+        <div>
+            <select onChange={(e) => {
+                setPaal(e.target.value);
+                setIyal(Object.keys(Meta[e.target.value] || {})[0] || "");
+            }} value={paal}>
+                {paalOptions.map(key => (
+                    <option key={key} value={key}>
+                        {key}
+                    </option>
+                ))}
+            </select>
+
+            <select onChange={(e) => setIyal(e.target.value)} value={iyal} disabled={!iyalOptions.length}>
+                {iyalOptions.map(key => (
+                    <option key={key} value={key}>
+                        {key}
+                    </option>
+                ))}
+            </select>
+
+            <select disabled={!itemOptions.length} onChange={(e) => props.changeKural(e.target.value)}>
+                {itemOptions.map(item => (
+                    <option key={item} value={item}>
+                        {item}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
--- src/components/RightBar/index.js
+++ src/components/RightBar/index.js
@@ -1,9 +1,15 @@
 import style from "./RightBar.module.css";
+import OptionsBanner from "../OptionsBanner";
+import Kural from "../Kural";
+import { useState } from "react";
 
 export default function Banner(){
+    const [ kural, setKural ] = useState("அகர முதல எழுத்தெல்லாம் ஆதி பகவன் முதற்றே உலகு");
     return (
         <div className={style.banner}>
             <h1 className={style.bannerHeading}>உயிர்க்குறள்</h1>
+            <OptionsBanner changeKural={setKural}/>
+            <Kural kural={kural}/>
         </div>
     )
 }

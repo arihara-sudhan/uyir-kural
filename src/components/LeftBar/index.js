de5994a6a2965e13fb54301fef91e2f5f50221d2
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-10-09 07:54:31 +0530

    Changes

--- src/components/LeftBar/index.js
+++ src/components/LeftBar/index.js
@@ -2,7 +2,9 @@ import style from "./LeftBar.module.css";
 
 export default function Banner(){
     return (
-        <div className={style.banner}>
-        </div>
+        <>
+            <div className={style.banner}>
+            </div>
+        </>
     )
 }
